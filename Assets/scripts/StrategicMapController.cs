a5cc731cbd3ba25e17a97392b29fc53e373ba45d
michael.landry <michael.landry@pps.io>
2020-05-22 19:01:19 -0400

    start combat

---
+++ Assets/scripts/StrategicMapController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StrategicMapController : MonoBehaviour
+{
+  public void GoToDungeon()
+  {
+    SceneManager.LoadScene("Dungeon");
+  }
+}
