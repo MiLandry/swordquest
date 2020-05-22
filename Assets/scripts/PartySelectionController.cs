4a68011fa9a99d940ffa80729791eb54c7e85cc6
michael.landry <michael.landry@pps.io>
2020-05-22 17:49:55 -0400

    init commit

---
+++ Assets/scripts/PartySelectionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PartySelectionController : MonoBehaviour
+{
+  public void SelectWarriorParty()
+  {
+    Debug.Log("aoei");
+    SceneManager.LoadScene("StrategicMap");
+  }
+}
