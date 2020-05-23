5600697330d926b2ee6672b3ed1c1899172feb29
michael.landry <michael.landry@pps.io>
2020-05-23 10:14:32 -0400

    dragdrop

---
+++ Assets/scripts/Hand.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+  //   private const int SLOTS = 5;
+
+  //   private List<ICard> cards = new List<ICard>();
+
+  //   public event EventHandler<CardEventArgs> CardAdded;
+
+  //   public ovoid AddCard(ICard card)
+  //   {
+  //     if (cards < SLOTS)
+  //     {
+  //       cards.Add(card);
+
+  //       card.onPickup();
+
+  //       if (CardAdded != null)
+  //       {
+  //         CardAdded(this, new HandEventArgs(card));
+  //       }
+
+  //     }
+  //   }
+
+
+}
---
+++ Assets/scripts/ICard.cs
@@ -0,0 +1,18 @@
+public interface ICard
+{
+  // string Name { get; }
+  // Sprite Image { get; }
+
+  // void OnDraw();
+
+}
+
+// public class CardEventArgs : CardEventArgs
+// {
+//   public CardEventArgs(ICard card)
+//   {
+//     CardEventArgs = card;
+//   }
+
+//   public ICard card;
+// }
